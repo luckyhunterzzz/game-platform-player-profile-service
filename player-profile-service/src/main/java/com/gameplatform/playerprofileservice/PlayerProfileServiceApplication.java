af634046e3b46a067ab219813a6a6c90d71c8929
Airat Zaripov <lucky_hunter@list.ru>
2026-03-14 13:11:35 +0100

    feature/GPPPS-1 - Init player-profile-service with Gradle and Spring Boot

---
+++ player-profile-service/src/main/java/com/gameplatform/playerprofileservice/PlayerProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gameplatform.playerprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerProfileServiceApplication.class, args);
+	}
+
+}
